435f1851a9288230ff5556ddfa9af58bacedf93d
jsgoldb <jgoldb@yahoo.com>
2016-05-19 17:32:49 -0500

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -10,7 +10,10 @@ function SomeDirective() {
 		},
 		controllerAs: 'some',
 		link: function (scope, elem, attrs) {
-
+			document.addEventListener('keypress', function(){
+				scope.counter += 1;
+				scope.$apply();
+			});
 		}
 	}
 }
