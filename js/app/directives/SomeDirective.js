35181b19fc9ef5ac35542b6c0ca340e448d5750c
Tyler Smith <oats000@hotmail.com>
2016-06-25 16:33:52 +0000

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,12 +5,20 @@ function SomeDirective() {
 				'<h3>{{ counter }}</h3>',
 			'</div>'
 		].join(''),
-		controller: function($scope) {
-			$scope.counter = 0;
+		require: 'someDirective',
+		controller: function() {
+			this.counter = 0;
 		},
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+			var actualElement = elem[0];
+			var h3Element = actualElement.querySelector('h3');
 
+			h3Element.addEventListener('click', function () {
+					ctrl.counter ++;
+
+					scope.$apply();
+			});
 		}
 	}
 }
