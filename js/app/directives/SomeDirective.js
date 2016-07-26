791544586ef4b79dfb89c742efebed9ae32594bd
thebrianemory <the@brianemory.com>
2016-07-26 10:18:07 -0700

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -2,15 +2,22 @@ function SomeDirective() {
 	return {
 		template: [
 			'<div>',
-				'<h3>{{ counter }}</h3>',
+				'<h3>{{ some.counter }}</h3>',
 			'</div>'
 		].join(''),
-		controller: function($scope) {
-			$scope.counter = 0;
+    require: 'someDirective',
+		controller: function() {
+			this.counter = 0;
 		},
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+      var actualElement = elem[0];
 
+      document.addEventListener('keypress', function () {
+        ctrl.counter += 1;
+
+        scope.$apply();
+      })
 		}
 	}
 }
