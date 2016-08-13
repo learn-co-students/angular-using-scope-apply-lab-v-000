c893fb31d88cd03425146d5ce9cf04a1522ed3e3
Vannak Chhay <vannak.chhay1@gmail.com>
2016-08-13 11:41:58 -0400

    done

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -10,7 +10,10 @@ function SomeDirective() {
 		},
 		controllerAs: 'some',
 		link: function (scope, elem, attrs) {
-
+							document.addEventListener('keydown', function () {
+							ctrl.counter++;
+							scope.$apply();
+						});
 		}
 	}
 }
