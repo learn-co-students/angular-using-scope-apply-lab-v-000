176b6a860f08515420ca8b47a7eb9183c199771a
github username <github email address>
2016-11-21 11:12:13 -0500

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -2,18 +2,24 @@ function SomeDirective() {
 	return {
 		template: [
 			'<div>',
-				'<h3>{{ counter }}</h3>',
+				'<h3>{{ some.counter }}</h3>',
 			'</div>'
 		].join(''),
-		controller: function($scope) {
-			$scope.counter = 0;
+		controller: function() {
+			this.counter = 0;
 		},
+		require: 'someDirective',
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+			document.addEventListener("keydown", function() {
+				ctrl.counter +=1;
+				scope.$apply();
+			})
 
+			}
 		}
 	}
-}
+
 
 angular
 	.module('app')
