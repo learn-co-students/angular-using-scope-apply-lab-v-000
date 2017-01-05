e9c2d858a650b022941bb76b9abf6e0b3d17064c
tpstar <hanlee12@gmail.com>
2017-01-04 23:52:06 -0600

    done

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -9,7 +9,16 @@ function SomeDirective() {
 			$scope.counter = 0;
 		},
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+			var actualElement = elem[0];
+
+			var spanElement = actualElement.querySelector('h3');
+
+			document.addEventListener('keydown', function() {
+				scope.counter += 1;
+
+				scope.$apply();
+			})
 
 		}
 	}
